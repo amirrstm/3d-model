f3117084ccf2a588a7530a5630c8043db26c853b
Amir Rostami <a.rostami@farazholding.com>
2020-10-30 15:32:24 +0330

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static('public'))
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/hamdast', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/hamdast.html'));
+});
+
+app.get('/expo', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/expo.html'));
+});
+
+app.get('/energy', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/energy.html'));
+});
+
+app.get('/gaz', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/gaz.html'));
+});
+
+
+app.listen(8080);
